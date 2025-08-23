a062a084d2ec859f527c88e3f5fc3c4835dfb549
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2025-08-22 20:16:14 -0700

    Added auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request; 
 use App\Models\WritingSession;
 
@@ -11,6 +13,13 @@ Route::get('/', function () {
         ->orderBy('day')
         ->get();
     return view('welcome', compact('writingSessions', 'daily'));
+});
+
+
+Route::get('/whoami', function () {
+    return auth()->check()
+        ? 'Logged in as: ' . auth()->user()->email
+        : 'Not logged in';
 });
 
 Route::post('/sessions', function (Request $request) {
@@ -26,3 +35,15 @@ Route::post('/sessions', function (Request $request) {
 
     return redirect('/')->with('success', 'Writing session created!');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
